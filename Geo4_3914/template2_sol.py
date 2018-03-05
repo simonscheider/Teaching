e8f1fb4c47dc5500d1c007a05f9696b99988ebe2
Scheider <s.scheider@uu.nl>
2018-03-05 20:06:45 +0100

    shapefile added

--- Geo4_3914/template2_sol.py
+++ Geo4_3914/template2_sol.py
@@ -12,7 +12,9 @@
 
 
 
-
+import arcpy
+import numpy
+import os
 import json, requests
 from nltk.corpus import stopwords
 from nltk import wordpunct_tokenize
@@ -101,6 +103,7 @@ def getFSPlaces(city = 'Utrecht, NL', section='food', limit=50):
             fp.seek(0)
             json.dump(out, fp)
     fp.close()
+    return outfile
 
 def findLanguage(text):
     languages_ratios = {}
@@ -115,10 +118,97 @@ def findLanguage(text):
         languages_ratios
     return max(languages_ratios, key=languages_ratios.get)
 
+def createGeometry(element, rs):
+    geom = arcpy.PointGeometry(arcpy.Point(float(element['lon']), float(element['lat'])),arcpy.SpatialReference(4326)).projectAs(rs)
+    return geom
+
+def json2SHP(jsonfile, outFC, keylist, rs):
+     #This genereates the output feature class
+        arcpy.CreateFeatureclass_management(os.path.dirname(outFC), os.path.basename(outFC), 'POINT', '', '', '', rs)
+
+        # Join fields to the feature class, using ExtendTable, depending on the OSM tags that came with the loaded results
+        print keylist
+        ff = (lambda s: (str(arcpy.ValidateFieldName((s[0:4])+(s[-5:]) if len(s) >= 10 else s))).upper())
+        #This makes sure the tag names are converted into valid fieldnames (of length 10 max), where the first and the last 5 characters are taken to build the string
+        tag_fields = map(ff, keylist)
+        print tag_fields
+
+        field_array = [('intfield', numpy.int32),
+                        ('Name_d', '|S255')
+                        ]
+        for f in tag_fields:
+            field_array.append((f, '|S255'))
+
+        #print field_array
+        inarray = numpy.array([],
+                          numpy.dtype(field_array))
+        #print field_array
+        arcpy.da.ExtendTable(outFC, "OID@", inarray, "intfield")
+
+
+        field_list = ['Name_d', 'SHAPE@']
+        field_list.extend(tag_fields)
+        #print field_list
+        rowsDA = arcpy.da.InsertCursor(outFC, field_list)
+
+        #Geometries and attributes are inserted
+        for obj in jsonfile:
+            geom = createGeometry(obj, rs)
+            f = lambda tag: obj.get(tag, "n/a")
+            tag_values = map(f,keylist)
+            #print tag_values
+            l = [obj.get("Name", "n/a"), geom]
+            l.extend(tag_values)
+            try:
+              rowsDA.insertRow(l)
+            except RuntimeError, e:
+              arcpy.AddError(str(e))
+        if rowsDA:
+            del rowsDA
+
+"""This method has a municipality name as input, selects the mun. object in a municipality layer, and stores the single municipality into a shapefile"""
+def getMunicipality(gemname, filen=r"C:\Temp\MTGIS\wijkenbuurten2017\gem_2017.shp", fieldname = "GM_NAAM"):
+    print ("Getting data for "+gemname)
+    out = os.path.join(arcpy.env.workspace, gemname+'.shp')
+    arcpy.MakeFeatureLayer_management(filen, 'municipalities_l')
+    arcpy.SelectLayerByAttribute_management('municipalities_l', where_clause=fieldname+"= '"+gemname+"' ")
+    arcpy.CopyFeatures_management('municipalities_l', out)
+    return out
+
+"""This method has a (municipality) shapefile as input, then gets its reference system (rs), gets the first geometry's extent, reprojects it to WGS 84, and returns a bbox, the rs and the extent"""
+def getBBfromFile(filen):
+    print ("Getting BB")
+    rs = arcpy.Describe(filen).spatialReference
+    sc = arcpy.da.SearchCursor(filen, ["SHAPE@"])
+    geom = sc.next()[0]
+    ext = geom.extent.projectAs("WGS 1984")
+    bbox = ", ".join(str(e) for e in [ext.YMin,ext.XMin,ext.YMax,ext.XMax])
+    return (bbox, rs, ext)
+
+
 
 
 def main():
-    getFSPlaces()
+    #0) Setting computing environment
+    arcpy.env.overwriteOutput = True #Such that files can be overwritten
+    arcpy.env.workspace = r"C:\Temp\MTWEB" #Setting the workspace
+    if arcpy.CheckExtension("Spatial") == "Available": #Check out spatial analyst extension
+        arcpy.CheckOutExtension("Spatial")
+
+    #1) Getting city municipality file for city outline (bounding box), and setting the processing extent
+    municipality = getMunicipality("Utrecht", filen=r"C:\Temp\MTWEB\wijkenbuurten2017\gem_2017.shp", fieldname = "GM_NAAM")
+    b = getBBfromFile(municipality)
+    bb = b[0] #Get the bounding box string in WGS 84
+    rs = b[1] #Get the reference system of the original municipality file
+    ext = b[2] #Get the extent object in the original reference system
+    arcpy.env.extent = ext  #Set the geoprocessing extent to the municipality outline in the origina
+
+    #jsonfile = getFSPlaces()
+    jsonfile = 'Utrecht, NLfoodfsdata.json'
+    tname = os.path.join(arcpy.env.workspace,r"result.shp")
+    with open(jsonfile) as data:
+        d = json.load(data)
+        json2SHP(d, tname,['cat','rating'],rs)
 
 
 
